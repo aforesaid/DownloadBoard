7ec645e9e20e24df1785ba63d61ab73fde935964
Yury <y.fomenkov@indeepa.com>
2021-09-11 23:01:04 +0300

    Refactoring

--- src/DownloadBoard/Program.cs
+++ src/DownloadBoard/Program.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Linq;
-using System.Threading.Tasks;
-using DownloadBoard.Models;
+﻿using System.Threading.Tasks;
 using DownloadBoard.Services;
 using Microsoft.Extensions.Configuration;
 
